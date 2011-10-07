fpoirotte/phpfarm

commit 5cdd3c666f3e2e41ec2d2e2d37cfb234b0690145

Enable PEAR.
PEAR is now enabled by default. You must pass --without-pear explicitely
in your $configoptions if you don't want it.
This installs 3 new commands (pear, peardev, pecl) in inst/bin/
for every compiled version of PHP where PEAR support was enabled.

src/compile.sh
@@ -114,7 +114,7 @@ cd "$srcdir"
  --exec-prefix="$instdir" \
  --enable-debug \
  --disable-short-tags \
- --without-pear \
+ --with-pear="$instdir/pear" \
  $configoptions
 
 if [ $? -gt 0 ]; then
@@ -197,6 +197,13 @@ fi
 
 ln -fs "$instdir/bin/php-config" "$shbindir/php-config-$version"
 ln -fs "$instdir/bin/phpize" "$shbindir/phpize-$version"
+# If PEAR was installed, finish the setup here.
+if [ -f "$shbindir/pear-$version" -o -L "$shbindir/pear-$version" ]; then
+    ln -fs "$instdir/bin/pear" "$shbindir/pear-$version"
+    ln -fs "$instdir/bin/peardev" "$shbindir/peardev-$version"
+    ln -fs "$instdir/bin/pecl" "$shbindir/pecl-$version"
+    "$instdir/bin/pear" config-set php_dir "$instdir/pear/php" system
+fi
 
 cd "$basedir"
 ./pyrus.sh "$version" "$instdir"

README
@@ -9,7 +9,7 @@ The PHP source packages are fetched from ``museum.php.net`` (which is not
 always up-to-date), the official php.net download pages and the
 pre-release channels.
 If a file cannot be found, try to fetch it manually and put it into
-``src/bzips/``
+``src/bzips/``.
 
 
 Setup
@@ -59,3 +59,17 @@ variables are substitued:
   Xdebug as ``extension_dir`` is not automatically prepended to the
   path for those extensions.
 
+
+Known issues
+------------
+The following entries are known issues which may or may not be solved
+in the future:
+
+- Do not use ``--enable-sigchld`` in your custom options if you plan
+  to install extensions using pear/pecl. When enabled, this option
+  will result in a failure during the ``phpize`` step (this issue
+  lies in PHP itself and is not specific to phpfarm).
+
+- By default, a (local) PEAR installation is created for every PHP version
+  you build. If you don't plan to use PEAR, you can prevent this from
+  happening by adding ``--without-pear`` to your ``$configoptions``.

commit a43d593c28ce36ebaf7cbf26d7f20b87c796ee6b

Fix typo that prevented pear from being installed.
A typo in a test condition introduced by commit 5cdd3c6 prevented
pear-related commands (pear, peardev, pecl) from being properly
installed.

src/compile.sh
@@ -217,7 +217,7 @@ for version in "${versions[@]}"; do
     ln -fs "$instdir/bin/php-config" "$shbindir/php-config-$version"
     ln -fs "$instdir/bin/phpize" "$shbindir/phpize-$version"
     # If PEAR was installed, finish the setup here.
-    if [ -f "$shbindir/pear-$version" -o -L "$shbindir/pear-$version" ]; then
+    if [ -f "$instdir/bin/pear" -o -L "$instdir/bin/pear" ]; then
         ln -fs "$instdir/bin/pear" "$shbindir/pear-$version"
         ln -fs "$instdir/bin/peardev" "$shbindir/peardev-$version"
         ln -fs "$instdir/bin/pecl" "$shbindir/pecl-$version"