f1411416e525e4b46531cce18cb775527e3629d7
François Poirotte <clicky@erebot.net>
2011-10-07 18:39:02 +0200

    Allow multiple successive builds.
    Multiple version names can now be passed to compile.sh::
    
      ./compile.sh  5.3.8  5.4.0beta1
    
    Those versions will be compiled & installed in the order given.

--- src/compile.sh
+++ src/compile.sh
@@ -25,191 +25,201 @@
 # Author: Christian Weiske <cweiske@php.net>
 #
 
-version=$1
-vmajor=`echo ${version%%.*}`
-vminor=`echo ${version%.*}`
-vminor=`echo ${vminor#*.}`
-vpatch=`echo ${version##*.}`
-
 #directory of this file. all php srces are extrated in it
 basedir="`dirname "$0"`"
 cd "$basedir"
 basedir=`pwd`
-#directory of php sources of specific version
-srcdir="php-$version"
-#directory with source archives
-bzipsdir='bzips'
-#directory phps get installed into
-instbasedir="`readlink -f "$basedir/../inst"`"
-#directory this specific version gets installed into
-instdir="$instbasedir/php-$version"
-#directory where all bins are symlinked
-shbindir="$instbasedir/bin"
 
-#we need a php version
-if [ "x$version" = 'x' ]; then
+versions=()
+for arg; do
+    if [ "x$arg" != "x" ]; then
+        echo "arg: $arg"
+        versions[${#versions[@]}]="$arg"
+    fi
+done
+
+if [ ${#versions[@]} -eq 0 ]; then
     echo 'Please specify php version'
     exit 1
 fi
 
-#already extracted?
-if [ ! -d "$srcdir" ]; then
-    echo 'Source directory does not exist; trying to extract'
-    srcfile="$bzipsdir/php-$version.tar.bz2"
-    if [ ! -f "$srcfile" -a ! -L "$srcfile" ]; then
-        echo 'Source file not found:'
-        echo "$srcfile"
-        url="http://museum.php.net/php$vmajor/php-$version.tar.bz2"
-        wget -P "$bzipsdir" "$url"
-        if [ ! -f "$srcfile" ]; then
-            echo "Fetching sources from museum failed"
-            echo $url
-            #museum failed, now we try real download
-            url="http://www.php.net/get/php-$version.tar.bz2/from/this/mirror"
-            wget -P "$bzipsdir" -O "$srcfile" "$url"
-        fi
-        if [ ! -s "$srcfile" -a -f "$srcfile" ]; then
-            rm "$srcfile"
-        fi
+for version in "${versions[@]}"; do
+    vmajor=`echo ${version%%.*}`
+    vminor=`echo ${version%.*}`
+    vminor=`echo ${vminor#*.}`
+    vpatch=`echo ${version##*.}`
 
-        if [ ! -f "$srcfile" ]; then
-            echo "Fetching sources from official download site failed"
-            echo $url
-            #use ilia's RC (5.3.x)
-            url="https://downloads.php.net/ilia/php-$version.tar.bz2"
-            wget -P "$bzipsdir" -O "$srcfile" "$url"
-        fi
-        if [ ! -s "$srcfile" -a -f "$srcfile" ]; then
-            rm "$srcfile"
-        fi
+    #directory of php sources of specific version
+    srcdir="php-$version"
+    #directory with source archives
+    bzipsdir='bzips'
+    #directory phps get installed into
+    instbasedir="`readlink -f "$basedir/../inst"`"
+    #directory this specific version gets installed into
+    instdir="$instbasedir/php-$version"
+    #directory where all bins are symlinked
+    shbindir="$instbasedir/bin"
 
-        if [ ! -f "$srcfile" ]; then
-            echo "Fetching sources from ilia's site failed"
-            echo $url
-            #use stas's RC (5.4.x)
-            url="https://downloads.php.net/stas/php-$version.tar.bz2"
-            wget -P "$bzipsdir" -O "$srcfile" "$url"
-        fi
-        if [ ! -s "$srcfile" -a -f "$srcfile" ]; then
-            rm "$srcfile"
-        fi
+    #already extracted?
+    if [ ! -d "$srcdir" ]; then
+        echo 'Source directory does not exist; trying to extract'
+        srcfile="$bzipsdir/php-$version.tar.bz2"
+        if [ ! -f "$srcfile" -a ! -L "$srcfile" ]; then
+            echo 'Source file not found:'
+            echo "$srcfile"
+            url="http://museum.php.net/php$vmajor/php-$version.tar.bz2"
+            wget -P "$bzipsdir" "$url"
+            if [ ! -f "$srcfile" ]; then
+                echo "Fetching sources from museum failed"
+                echo $url
+                #museum failed, now we try real download
+                url="http://www.php.net/get/php-$version.tar.bz2/from/this/mirror"
+                wget -P "$bzipsdir" -O "$srcfile" "$url"
+            fi
+            if [ ! -s "$srcfile" -a -f "$srcfile" ]; then
+                rm "$srcfile"
+            fi
 
-        if [ ! -f "$srcfile" ]; then
-            echo "Fetching sources failed:"
-            echo $url
-            exit 2
+            if [ ! -f "$srcfile" ]; then
+                echo "Fetching sources from official download site failed"
+                echo $url
+                #use ilia's RC (5.3.x)
+                url="https://downloads.php.net/ilia/php-$version.tar.bz2"
+                wget -P "$bzipsdir" -O "$srcfile" "$url"
+            fi
+            if [ ! -s "$srcfile" -a -f "$srcfile" ]; then
+                rm "$srcfile"
+            fi
+
+            if [ ! -f "$srcfile" ]; then
+                echo "Fetching sources from ilia's site failed"
+                echo $url
+                #use stas's RC (5.4.x)
+                url="https://downloads.php.net/stas/php-$version.tar.bz2"
+                wget -P "$bzipsdir" -O "$srcfile" "$url"
+            fi
+            if [ ! -s "$srcfile" -a -f "$srcfile" ]; then
+                rm "$srcfile"
+            fi
+
+            if [ ! -f "$srcfile" ]; then
+                echo "Fetching sources failed:"
+                echo $url
+                exit 2
+            fi
         fi
+        #extract
+        tar xjvf "$srcfile"
     fi
-    #extract
-    tar xjvf "$srcfile"
-fi
 
 
-source 'options.sh' "$version" "$vmajor" "$vminor" "$vpatch"
-cd "$srcdir"
-#configuring
-#TODO: do not configure when config.nice exists
-./configure \
- --prefix="$instdir" \
- --exec-prefix="$instdir" \
- --enable-debug \
- --disable-short-tags \
- --with-pear="$instdir/pear" \
- $configoptions
+    source 'options.sh' "$version" "$vmajor" "$vminor" "$vpatch"
+    cd "$srcdir"
+    #configuring
+    #TODO: do not configure when config.nice exists
+    ./configure \
+     --prefix="$instdir" \
+     --exec-prefix="$instdir" \
+     --enable-debug \
+     --disable-short-tags \
+     --with-pear="$instdir/pear" \
+     $configoptions
 
-if [ $? -gt 0 ]; then
-    echo configure.sh failed.
-    exit 3
-fi
+    if [ $? -gt 0 ]; then
+        echo configure.sh failed.
+        exit 3
+    fi
 
-#compile sources
-#make clean
-make
-if [ "$?" -gt 0 ]; then
-    echo make failed.
-    exit 4
-fi
+    #compile sources
+    #make clean
+    make
+    if [ "$?" -gt 0 ]; then
+        echo make failed.
+        exit 4
+    fi
 
-make install
-if [ "$?" -gt 0 ]; then
-    echo make install failed.
-    exit 5
-fi
+    make install
+    if [ "$?" -gt 0 ]; then
+        echo make install failed.
+        exit 5
+    fi
 
-#copy php.ini
-initarget="$instdir/lib/php.ini"
-if [ -f "php.ini-development" ]; then
-    #php 5.3
-    cp "php.ini-development" "$initarget"
-elif [ -f "php.ini-recommended" ]; then
-    #php 5.1, 5.2
-    cp "php.ini-recommended" "$initarget"
-else
-    echo "No php.ini file found."
-    echo "Please copy it manually to $instdir/lib/php.ini"
-fi
+    #copy php.ini
+    initarget="$instdir/lib/php.ini"
+    if [ -f "php.ini-development" ]; then
+        #php 5.3
+        cp "php.ini-development" "$initarget"
+    elif [ -f "php.ini-recommended" ]; then
+        #php 5.1, 5.2
+        cp "php.ini-recommended" "$initarget"
+    else
+        echo "No php.ini file found."
+        echo "Please copy it manually to $instdir/lib/php.ini"
+    fi
 
-#set default ini values
-cd "$basedir"
-if [ -f "$initarget" ]; then
-    #fixme: make the options unique or so
-    custom="custom-php.ini"
-    [ ! -f $custom -a ! -L $custom ] && cp "default-custom-php.ini" "$custom"
+    #set default ini values
+    cd "$basedir"
+    if [ -f "$initarget" ]; then
+        #fixme: make the options unique or so
+        custom="custom-php.ini"
+        [ ! -f $custom -a ! -L $custom ] && cp "default-custom-php.ini" "$custom"
 
-    ext_dir=`"$instdir/bin/pear" config-get ext_dir system`
+        ext_dir=`"$instdir/bin/pear" config-get ext_dir system`
+        for suffix in "" "-$vmajor" "-$vmajor.$vminor" "-$vmajor.$vminor.$vpatch"; do
+            custom="custom-php$suffix.ini"
+            [ -f $custom -o -L $custom ] && sed -e 's#$ext_dir#'"$ext_dir"'#' "$custom" >> "$initarget"
+        done
+    fi
+
+    #create bin
+    [ ! -d "$shbindir" ] && mkdir "$shbindir"
+    if [ ! -d "$shbindir" ]; then
+        echo "Cannot create shared bin dir"
+        exit 6
+    fi
+    #symlink all files
+
+    #php may be called php.gcno
+    bphp="$instdir/bin/php"
+    bphpgcno="$instdir/bin/php.gcno"
+    if [ -f "$bphp" ]; then
+        ln -fs "$bphp" "$shbindir/php-$version"
+    elif [ -f "$bphpgcno" ]; then
+        ln -fs "$bphpgcno" "$shbindir/php-$version"
+    else
+        echo "no php binary found"
+        exit 7
+    fi
+
+    #php-cgi may be called php.gcno
+    bphpcgi="$instdir/bin/php-cgi"
+    bphpcgigcno="$instdir/bin/php-cgi.gcno"
+    if [ -f "$bphpcgi" ]; then
+        ln -fs "$bphpcgi" "$shbindir/php-cgi-$version"
+    elif [ -f "$bphpcgigcno" ]; then
+        ln -fs "$bphpcgigcno" "$shbindir/php-cgi-$version"
+    else
+        echo "no php-cgi binary found"
+        exit 8
+    fi
+
+    ln -fs "$instdir/bin/php-config" "$shbindir/php-config-$version"
+    ln -fs "$instdir/bin/phpize" "$shbindir/phpize-$version"
+    # If PEAR was installed, finish the setup here.
+    if [ -f "$shbindir/pear-$version" -o -L "$shbindir/pear-$version" ]; then
+        ln -fs "$instdir/bin/pear" "$shbindir/pear-$version"
+        ln -fs "$instdir/bin/peardev" "$shbindir/peardev-$version"
+        ln -fs "$instdir/bin/pecl" "$shbindir/pecl-$version"
+        "$instdir/bin/pear" config-set php_dir "$instdir/pear/php" system
+    fi
+
+    cd "$basedir"
+    ./pyrus.sh "$version" "$instdir"
+
+    # Post-install stuff
     for suffix in "" "-$vmajor" "-$vmajor.$vminor" "-$vmajor.$vminor.$vpatch"; do
-        custom="custom-php$suffix.ini"
-        [ -f $custom -o -L $custom ] && sed -e 's#$ext_dir#'"$ext_dir"'#' "$custom" >> "$initarget"
+        post="./post-install$suffix.sh"
+        [ -f $post -o -L $post ] && /bin/bash "$post" "$version" "$instdir" "$shbindir"
     done
-fi
-
-#create bin
-[ ! -d "$shbindir" ] && mkdir "$shbindir"
-if [ ! -d "$shbindir" ]; then
-    echo "Cannot create shared bin dir"
-    exit 6
-fi
-#symlink all files
-
-#php may be called php.gcno
-bphp="$instdir/bin/php"
-bphpgcno="$instdir/bin/php.gcno"
-if [ -f "$bphp" ]; then
-    ln -fs "$bphp" "$shbindir/php-$version"
-elif [ -f "$bphpgcno" ]; then
-    ln -fs "$bphpgcno" "$shbindir/php-$version"
-else
-    echo "no php binary found"
-    exit 7
-fi
-
-#php-cgi may be called php.gcno
-bphpcgi="$instdir/bin/php-cgi"
-bphpcgigcno="$instdir/bin/php-cgi.gcno"
-if [ -f "$bphpcgi" ]; then
-    ln -fs "$bphpcgi" "$shbindir/php-cgi-$version"
-elif [ -f "$bphpcgigcno" ]; then
-    ln -fs "$bphpcgigcno" "$shbindir/php-cgi-$version"
-else
-    echo "no php-cgi binary found"
-    exit 8
-fi
-
-ln -fs "$instdir/bin/php-config" "$shbindir/php-config-$version"
-ln -fs "$instdir/bin/phpize" "$shbindir/phpize-$version"
-# If PEAR was installed, finish the setup here.
-if [ -f "$shbindir/pear-$version" -o -L "$shbindir/pear-$version" ]; then
-    ln -fs "$instdir/bin/pear" "$shbindir/pear-$version"
-    ln -fs "$instdir/bin/peardev" "$shbindir/peardev-$version"
-    ln -fs "$instdir/bin/pecl" "$shbindir/pecl-$version"
-    "$instdir/bin/pear" config-set php_dir "$instdir/pear/php" system
-fi
-
-cd "$basedir"
-./pyrus.sh "$version" "$instdir"
-
-# Post-install stuff
-for suffix in "" "-$vmajor" "-$vmajor.$vminor" "-$vmajor.$vminor.$vpatch"; do
-    post="./post-install$suffix.sh"
-    [ -f $post -o -L $post ] && /bin/bash "$post" "$version" "$instdir" "$shbindir"
 done
+exit 0
