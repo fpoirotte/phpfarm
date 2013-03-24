3bda06490e16714ae871e9c36ce6373710b74251
François Poirotte <clicky@erebot.net>
2013-03-24 10:30:50 +0100

    [BC] Change flags order and add flag for pear.
    The flags no appear in alphabetical order in the version string.
    A new "pear" flag has been added to indicate that a PEAR installation
    is required.

--- src/helpers.sh
+++ src/helpers.sh
@@ -7,7 +7,7 @@ parse_version() {
     # Inputs:
     # $1: version string to parse.
     #     Eg. "5.4.0RC1-debug-unknown_token"
-    #     Valid tokens: zts, debug, 32bits, gcov, suhosin.
+    #     Valid tokens: 32bits, debug, gcov, pear, suhosin, zts.
     #
     # Outputs:
     # $VMAJOR: major number (5)
@@ -18,6 +18,7 @@ parse_version() {
     # $ARCH32: set to 1 for a 32bits build on a 64bits machine
     # $GCOV: set to 1 if asked for GCOV support
     # $SUHOSIN: set to 1 if asked for Suhosin patch
+    # $PEAR: set to 1 if asked to install PEAR
     # $SHORT_VERSION: same as $VMAJOR.$VMINOR.$VPATCH
     # $VERSION: full version string, normalized.
     #           Eg. "5.4.0RC1-debug-zts-32bits-gcov"
@@ -36,18 +37,20 @@ parse_version() {
     v=`echo "$v" | cut -d ' ' -f 4-`
 
     # extract tokens & set flags accordingly
-    DEBUG=0
-    ZTS=0
     ARCH32=0
+    DEBUG=0
     GCOV=0
+    PEAR=0
     SUHOSIN=0
+    ZTS=0
     for p in $v; do
         case $p in
-            debug)      DEBUG=1;;
-            zts)        ZTS=1;;
             32bits)     ARCH32=1;;
+            debug)      DEBUG=1;;
             gcov)       GCOV=1;;
+            pear)       PEAR=1;;
             suhosin)    SUHOSIN=1;;
+            zts)        ZTS=1;;
             *)          echo "Unsupported token '$p'";
                         return 2;;
         esac
@@ -56,20 +59,20 @@ parse_version() {
     # normalize version string
     VERSION="$VMAJOR.$VMINOR.$VPATCH"
     SHORT_VERSION="$VERSION"
-    if [ $SUHOSIN = 1 ]; then
-        VERSION="$VERSION-suhosin"
+    if [ $ARCH32 = 1 ]; then
+        VERSION="$VERSION-32bits"
     fi
     if [ $DEBUG = 1 ]; then
         VERSION="$VERSION-debug"
     fi
-    if [ $ZTS = 1 ]; then
-        VERSION="$VERSION-zts"
-    fi
-    if [ $ARCH32 = 1 ]; then
-        VERSION="$VERSION-32bits"
-    fi
     if [ $GCOV = 1 ]; then
         VERSION="$VERSION-gcov"
+    fi
+    if [ $SUHOSIN = 1 ]; then
+        VERSION="$VERSION-suhosin"
+    fi
+    if [ $ZTS = 1 ]; then
+        VERSION="$VERSION-zts"
     fi
 
     return 0
