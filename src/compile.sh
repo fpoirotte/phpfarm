3208129e6f58ded59ddb6467d638f08ff5e6c4bd
François Poirotte <clicky@erebot.net>
2017-07-09 13:43:45 +0200

    Add Remi's repository to the downloads

--- src/compile.sh
+++ src/compile.sh
@@ -84,6 +84,7 @@ sources=(
     "https://downloads.php.net/~krakjoe/php-$SHORT_VERSION.tar.bz2"
     "https://downloads.php.net/~davey/php-$SHORT_VERSION.tar.bz2"
     "https://downloads.php.net/~pollita/php-$SHORT_VERSION.tar.bz2"
+    "https://downloads.php.net/~remi/php-$SHORT_VERSION.tar.bz2"
 )
 
 #already extracted?
